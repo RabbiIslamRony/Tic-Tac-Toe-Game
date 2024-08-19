221b52dedb160c31f93d814fa461476ec687a618
Rabbi Islam Rony ⚡️ <ronya4927@gmail.com>
2024-08-19 15:25:12 +0600

    added Tic-Tac-Toe Game

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -2,6 +2,90 @@
 //
 // You have two choices for including Bootstrap's JS files—the whole thing,
 // or just the bits that you need.
+const cells = document.querySelectorAll('.cell');
+const statusText = document.querySelector('.status');
+const restartButton = document.getElementById('restart');
+let currentPlayer = 'X';
+let gameActive = true;
+let gameState = ['', '', '', '', '', '', '', '', ''];
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const handleCellPlayed = (clickedCell, clickedCellIndex) => {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.textContent = currentPlayer;
+};
+
+const handlePlayerChange = () => {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    statusText.textContent = `It's ${currentPlayer}'s turn`;
+};
+
+const handleResultValidation = () => {
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        const winCondition = winningConditions[i];
+        const a = gameState[winCondition[0]];
+        const b = gameState[winCondition[1]];
+        const c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        statusText.textContent = `${currentPlayer} wins!`;
+        gameActive = false;
+        return;
+    }
+
+    const roundDraw = !gameState.includes('');
+    if (roundDraw) {
+        statusText.textContent = 'Draw!';
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+};
+
+const handleCellClick = (event) => {
+    const clickedCell = event.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index'));
+
+    if (gameState[clickedCellIndex] !== '' || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+};
+
+const handleRestartGame = () => {
+    gameActive = true;
+    currentPlayer = 'X';
+    gameState = ['', '', '', '', '', '', '', '', ''];
+    statusText.textContent = `It's ${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = '');
+};
+
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+restartButton.addEventListener('click', handleRestartGame);
+
+statusText.textContent = `It's ${currentPlayer}'s turn`;
 
 
 // Option 1
